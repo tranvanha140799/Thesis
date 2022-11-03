0959c9b31af26ff97f2b7c5a45556f0b50515081
Ha Tran Van <tranvanha140799@users.noreply.github.com>
2022-11-03 18:10:58 +0700

    Update gitignore

--- thesis-be/models/paySalary.js
+++ thesis-be/models/paySalary.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose';
 
 const paySalarySchema = mongoose.Schema({
-  period: String, // Chu kỳ (thời gian) trả lương (tháng)
+  period: String, // Thời gian trả lương - kỳ lương (tháng mấy??)
   salary: Number, // Lương
   isPaidSalary: Boolean, // Đã trả lương hay chưa?
-  datePaidSalary: String,
+  datePaidSalary: String, // Ngày trả lương
   advancePayment: Number, // Khoản lương thanh toán trước (ứng lương)
   teacherId: String,
 });
