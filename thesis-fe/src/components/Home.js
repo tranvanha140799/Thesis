87edcab73d0f873d827413ab9e867234fed7ddc2
Ha Tran Van <tranvanha140799@users.noreply.github.com>
2022-11-09 23:27:31 +0700

    Edit sidebar content

---
+++ thesis-fe/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div
+    style={{
+      textAlign: 'center',
+      fontWeight: 'revert-layer',
+      fontSize: 30,
+      lineHeight: '500px',
+    }}
+  >
+    Hệ Thống Quản Lý Thanh Toán Giảng Dạy & Thu Học Phí Của Trung Tâm Tiếng Anh
+  </div>
+);
+
+export default HomePage;
